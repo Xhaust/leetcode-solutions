06a0961316eb05827ac6b347b7bbcabcf01ff872
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-05-29 23:26:50 +0200

    Solve 1299. Replace Elements With Greatest Element on Right Side

---
+++ easy/1299.replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,13 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        
+    }
+};
+// @leet end
