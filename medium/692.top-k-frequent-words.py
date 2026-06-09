6896a38b9deb77460bba179de32b61971eb672f1
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-06-09 17:25:58 +0200

    Solve 692. Top K Frequent Words

---
+++ medium/692.top-k-frequent-words.py
@@ -0,0 +1,50 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
+        # O(n log n)
+
+        dict = Counter(words)
+        sorted_list = sorted(dict.items(), key=lambda word: (-word[1], word[0]))
+        sorted_list = [word[0] for word in sorted_list]
+
+        return sorted_list[:k]
+
+# @leet end
