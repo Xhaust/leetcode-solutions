2abc343bd12ef8b4d39307e98c77a871ddee3edc
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-06-14 11:57:01 +0200

    Solve 167. Two Sum II

---
+++ medium/167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,53 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        # O(n)
+
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum == target:
+                return [l + 1, r + 1]
+            elif sum < target: l += 1
+            else: r -= 1
+        
+# @leet end
