50ba83efd8d10ff6478398670163280d1cf40a7e
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-07-14 20:00:44 +0200

    Solve 102. Binary Tree Level Order Traversal

---
+++ medium/102.binary-tree-level-order-traversal.py
@@ -0,0 +1,70 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+        # O(n)
+
+        if root is None:
+            return []
+
+        res = []
+        queue = deque([root])
+        
+        while queue:
+            curr_level = []
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                curr_level.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+
+            res.append(curr_level)
+
+        return res
+        
+# @leet end
