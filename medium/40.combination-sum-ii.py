7a3fd696dc40fd0388a47ecbb6657a7cb9c3fa08
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-08-07 22:45:51 +0200

    Solve 40. Combination Sum II

---
+++ medium/40.combination-sum-ii.py
@@ -0,0 +1,66 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        # O(2^n) | WATCHED SOLUTION
+
+        candidates.sort()
+        res = []
+
+        def backtrack(state, total, start):
+
+            if total > target:
+                return
+            
+            if total == target:
+                res.append(list(state))
+
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                state.append(candidates[i])
+                backtrack(state, total + candidates[i], i + 1)
+                state.pop()
+
+        backtrack([], 0, 0)
+
+        return res
+        
+# @leet end
