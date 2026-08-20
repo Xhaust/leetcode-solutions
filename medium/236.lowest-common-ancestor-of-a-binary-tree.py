15f04c528e8b85bed07d89d00a061b6ff73dff26
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-08-20 03:28:08 +0200

    Solve 236. Lowest COmmon Ancestor of a Binary Tree

--- medium/236.lowest-common-ancestor-of-a-binary-tree.py
+++ medium/236.lowest-common-ancestor-of-a-binary-tree.py
@@ -37,11 +37,11 @@ from typing import *
 
 # @leet start
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
@@ -56,6 +56,7 @@ class Solution:
 
         if l and r:
             return root
-        
+
         return l if l else r
+        
 # @leet end
