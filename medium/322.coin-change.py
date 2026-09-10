2b4493144df39c5cda383e93ef58bdbe899b5f21
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-09-10 22:46:08 +0200

    Solve 322. Coin Change

---
+++ medium/322.coin-change.py
@@ -0,0 +1,54 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        # O(nm) | WATCHED SOLUTION
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return dp[amount] if dp[amount] != float('inf') else -1
+        
+# @leet end
