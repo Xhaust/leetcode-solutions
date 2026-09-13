512939d54cb979763816f1039e5b3ab5f139d19f
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-09-13 23:29:48 +0200

    Solve 5. Longest Palindromic Substring

--- medium/5.longest-palindromic-substring.py
+++ medium/5.longest-palindromic-substring.py
@@ -39,28 +39,32 @@ from typing import *
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
-        # O(n) | WATCHED SOLUTION
+        # O(n)
 
+        longest = 0
         res = ""
-        res_len = 0
 
-        for i in range(len(s)):
-            l, r = i, i
+        for i, char in enumerate(s):
+            l = i
+            r = i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > res_len:
-                    res = s[l:r+1]
-                    res_len = r - l + 1
+                if (r - l + 1) > longest:
+                    longest = r - l + 1
+                    res = s[l : r + 1]
                 l -= 1
                 r += 1
 
-            l, r = i, i + 1
+            l = i
+            r = i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > res_len:
-                    res = s[l:r+1]
-                    res_len = r - l + 1
+                if (r - l + 1) > longest:
+                    longest = r - l + 1
+                    res = s[l : r + 1]
                 l -= 1
                 r += 1
 
         return res
+
+
         
 # @leet end
