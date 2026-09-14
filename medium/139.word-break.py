dc138cb874d66ce08ac333d959f45e959040057e
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-09-14 21:01:01 +0200

    Solve 139. Word Break

---
+++ medium/139.word-break.py
@@ -0,0 +1,58 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        # O(n)
+
+        words = set(wordDict)
+
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in words:
+                    dp[i] = True
+                    break
+
+        return dp[len(s)]
+
+         
+# @leet end
