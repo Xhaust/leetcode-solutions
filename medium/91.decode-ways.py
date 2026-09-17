afab1810c9aab8098ccbf6ff6aac04ca35ddd0c4
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-09-17 22:59:14 +0200

    Solve 91. Decode Ways

--- medium/91.decode-ways.py
+++ medium/91.decode-ways.py
@@ -39,32 +39,24 @@ from typing import *
 class Solution:
     def numDecodings(self, s: str) -> int:
 
-        # O(n) | TOP DOWN APPROACH
+        # O(n) | TOP DOWN APPROACH (WATCHED SOLUTION)
 
-        memo = {}
+        memo = {len(s): 1}
 
         def dfs(i):
-            if i >= len(s):
-                return 1
-
+            if i in memo:
+                return memo[i]
             if s[i] == '0':
                 return 0
 
-            if i in memo:
-                return memo[i]
-
             res = dfs(i + 1)
 
-            if i + 1 < len(s):
-                decimal = s[i:i+2]
-                if "10" <= decimal <= "26":
-                    res += dfs(i + 2)
+            if i + 1 < len(s) and (s[i] == '1' or (s[i] == '2' and s[i + 1] in "0123456")):
+                res += dfs(i + 2)
 
             memo[i] = res
-
             return res
-
+        
         return dfs(0)
-
         
 # @leet end
